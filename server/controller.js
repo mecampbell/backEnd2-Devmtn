bbd662f35c795ec4227e1da0fca4e63f8224ba2e
Michael Campbell <michaelmichael323@hotmail.com>
2022-05-21 14:41:31 -0600

    finished

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,7 @@ module.exports = {
     getHouses: (req, res) => res.status(200).send(houses),
     deleteHouse: (req, res) => {
         let index = houses.findIndex(elem => elem.id === +req.params.id);
-        movies.splice(index, 1);
+        houses.splice(index, 1);
         res.status(200).send(houses);
     },
     createHouse: (req, res) => {
@@ -23,17 +23,16 @@ module.exports = {
     updateHouse: (req, res) => {
         let { id } = req.params;
         let { type } = req.body;
-        let newPrice = houses[index].price;
         let index = houses.findIndex(elem => +elem.id === +id)
 // use a sequce of conditional checks to see if the type is ‘minus’ or ‘plus’, and then decrease or increase the price of the hosue by $10,000.
-        if (newPrice <= 10000 && type === 'minus') {
-            newPrice = 0;
+        if (houses[index].price <= 10000 && type === 'minus') {
+            houses[index].price = 0;
             res.status(200).send(houses);
         } else if (type === 'plus') {
-            newPrice += 10000;
+            houses[index].price += 10000;
             res.status(200).send(houses);
         } else if (type === 'minus') {
-            newPrice -= 10000;
+            houses[index].price -= 10000;
             res.status(200).send(houses);
         } else {
             res.sendStatus(400);
